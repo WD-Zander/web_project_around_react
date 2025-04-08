587d5f14d4992a5fc3ea911e071c6096474b7e70
Zander <zanderjosue05.com>
2025-04-08 16:30:14 -0400

    Add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+    <p className="footer__copyright">© 2025 Zander Garcia - Aroud The U.S.</p>
+  </footer>
+    )
+}
+
+ 
