ee0239d6b487b72473283d93f2cae661e788552e
Zander <zanderjosue05.com>
2025-04-08 16:28:47 -0400

    Remove Footer component

--- src/components/Footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="footer">
-    <p className="footer__copyright">© 2025 Zander Garcia - Aroud The U.S.</p>
-  </footer>
-    )
-}
-
- 
